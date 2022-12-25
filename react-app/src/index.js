665c336ca20c5efedb3eea01e9ab4e0c384d6b6a
jianhui <liujh1@fosun.com>
2022-12-25 14:22:42 +0800

    单向链表

---
+++ react-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './链表'
+
+const App = () => {
+  return <div>app</div>
+}
+
+const container = document.getElementById('root')
+ReactDOM.render(<App/>, container)
